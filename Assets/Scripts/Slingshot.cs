5c082245235926241933584bab1acd8b05e02854
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-02 19:05:12 -0400

    Slingshot
    Added the mouse responsiveness. When clicked/unclicked

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -13,6 +13,7 @@ public class Slingshot : MonoBehaviour
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+    private Rigidbody projectileRigidbody;
 
     void Awake()
     {
@@ -42,5 +43,37 @@ public class Slingshot : MonoBehaviour
         projectile.transform.position = launchPos;
         //Set it to is Kinesmatic
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+        //set it to is kinesmatic
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+    void Update()
+    {
+        if (!aimingMode) return;
+        //get the current mouse porition in 2D screen
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //find the delta from the launch pos to the mousepos3d
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        //limit mouse delta to the radius of the alingshot SphereCollider.
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        //move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            //the mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 }
