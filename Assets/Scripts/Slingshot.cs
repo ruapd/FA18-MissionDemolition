91c6d7e76219aae4b896c8b9d3232f9b4e831fa4
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-01 21:30:06 -0400

    SlingShot Script
    added boolean values

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter() 
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+	}
+
+    private void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
