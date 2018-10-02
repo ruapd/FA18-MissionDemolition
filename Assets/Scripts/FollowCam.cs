00d2b08e5d2c76229c0173c41a8d8c812904b8b5
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-02 19:48:43 -0400

    FollowCam
    the Camera now Follows the projectile.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour {
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minxY = Vector2.zero;
+
     [Header("Set Dynamically")]
     public float camZ;
 
@@ -26,8 +30,14 @@ public class FollowCam : MonoBehaviour {
 
         //get the position of the POI
         Vector3 destination = POI.transform.position;
+        //limit the x and Y to min values
+        destination.x = Mathf.Max(minxY.x, destination.x);
+        destination.y = Mathf.Max(minxY.y, destination.y);
+        //Interpolate from the current cCamera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         //force destination.z to be camZ to keep the camera gar enough away
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
 	}
 }
